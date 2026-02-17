652e89d2c6d462e9acd362dfad9b0a840b79f8b6
Zachary Wooten <zbwoote@clemson.edu>
2026-02-17 16:07:53 -0500

    Finished 2.9
    Added case-insensitive search bar for names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,33 +1,68 @@
 import { useState } from 'react'
 
-const Person = ({name}) => <p>{name}</p>
+const Person = ({name, number}) => <p>{name} {number}</p>
 
 function App() {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
-  ])
-  const [newName, setNewName] = useState('')
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [newSearchFilter, setNewSearchFilter] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = {
-      name: newName
+    
+    const hasDuplicateName = persons.some(person => person.name.toLowerCase() === newName.toLowerCase());
+    const hasDuplicateNumber = persons.some(person => person.number === newNumber);
+
+    if (hasDuplicateName  || hasDuplicateNumber) {
+      alert(`${newName} or ${newNumber} already exists`);
+      return;
     }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    setNewNumber('');
   }
 
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(newSearchFilter.toLowerCase())
+  );
+
   const handlePersonChange = (event) => {
-    setNewName(event.target.value)
+    setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  }
+
+  const handleSearchFilter = (event) => {
+    setNewSearchFilter(event.target.value);
   }
 
   return (
     <>
       <div>
         <h2>Phonebook</h2>
+        <form className="searchPhonebook">
+          filter show with <input type="text" value={newSearchFilter} onChange={handleSearchFilter} />
+        </form>
         <form onSubmit={addPerson}>
           <div>
             name: <input value={newName} onChange={handlePersonChange}/>
+          </div>
+          <div>
+            number: <input value={newNumber} onChange={handleNumberChange}/>
           </div>
           <div>
             <button type="submit">add</button>
@@ -35,8 +70,8 @@ function App() {
         </form>
         <h2>Numbers</h2>
           <ul>
-            {persons.map(person =>
-              <Person key={person.name} name={person.name} />
+            {filteredPersons.map(person =>
+              <Person key={person.name} name={person.name} number={person.number}/>
             )}
           </ul>
       </div>
