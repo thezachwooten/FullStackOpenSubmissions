35a524c65329a774d71a843b03920127c7edfc97
Zachary Wooten <zbwoote@clemson.edu>
2026-03-19 18:47:06 -0400

    2.16 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,17 @@ import axios from 'axios';
 import PersonForm from './components/PersonForm.jsx';
 import FilterForm from './components/FilterForm.jsx';
 import Persons from './components/Persons.jsx';
-import phoneService from './services/numbers.js'
+import phoneService from './services/numbers.js';
+import Notification from './components/Notification.jsx';
+
+import './styles/index.css';
 
 function App() {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newSearchFilter, setNewSearchFilter] = useState('');
+  const [notification, setNotification] = useState(null);
 
   // UseEffect to use phoneService to getAll of perons from db instead of doing the axios calls here. GetAll returns a promise 
   useEffect(() => {
@@ -18,7 +22,7 @@ function App() {
       .then(initialNotes => {
         setPersons(initialNotes)
       })
-  }, [])
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,6 +40,10 @@ function App() {
           .updatePerson(curId, curName, newNumber)
           .then(updatedPerson => {
               setPersons(persons.map(p => p.id === updatedPerson.id ? updatedPerson : p))
+              setNotification(`${curName} number updated to ${newNumber}`)
+              setTimeout(() => {
+                setNotification(null)
+              }, 5000)
               setNewName('')
               setNewNumber('')
           })
@@ -56,10 +64,14 @@ function App() {
       .create(newPerson)
       .then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
+        setNotification(`${newPerson.name} added with number: ${newPerson.number}`)
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
         setNewName('')
         setNewNumber('')
       })
-  }
+  };
 
   const filteredPersons =  persons ? persons.filter(person =>
     person.name.toLowerCase().includes(newSearchFilter.toLowerCase())
@@ -67,15 +79,15 @@ function App() {
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value);
-  }
+  };
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
 
   const handleSearchFilter = (event) => {
     setNewSearchFilter(event.target.value);
-  }
+  };
 
   const removePerson = (id, name) => {
     const result = confirm(`Are you sure you want to delete ${name}?`)
@@ -85,12 +97,13 @@ function App() {
       .then(returnedData => setPersons(persons.filter(n => n.id !== returnedData.id)))
       .catch(err => alert(err))
     }
-  }
+  };
 
   return (
     <>
       <div>
         <h2>Phonebook</h2>
+        {notification && <Notification message={notification}/>}
         <FilterForm className="searchFilter" newSearchFilter={newSearchFilter} handleSearchFilter={handleSearchFilter}/>
         <PersonForm addPerson={addPerson} newName={newName} handlePersonChange={handlePersonChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
         <h2>Numbers</h2>
